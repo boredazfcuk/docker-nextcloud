6139901488f342fa468271b4717bdfeef234ce29
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-17 23:03:39 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,22 @@
 #!/bin/ash
 
-php -v > /dev/null || exit 1
-#/usr/bin/mysql --protocol=tcp -h $(grep dbhost /var/www/html/$(if [ "${nextcloud_base_dir}" ]; then echo "${nextcloud_base_dir}/"; fi)config/config.php | cut -d"'" -f4) -u $(grep dbuser /var/www/html/$(if [ "${nextcloud_base_dir}" ]; then echo "${nextcloud_base_dir}/"; fi)config/config.php | cut -d"'" -f4) -p$(grep dbpassword /var/www/html/$(if [ "${nextcloud_base_dir}" ]; then echo "${nextcloud_base_dir}/"; fi)config/config.php | cut -d"'" -f4) -e "USE $(grep dbname /var/www/html/$(if [ "${nextcloud_base_dir}" ]; then echo "${nextcloud_base_dir}/"; fi)config/config.php | cut -d"'" -f4);" || exit 1
-SCRIPT_NAME=/status; SCRIPT_FILENAME=/usr/local/php/php/fpm/status.html; REQUEST_METHOD=GET; cgi-fcgi -bind -connect "${HOSTNAME}:9000" >/dev/null || exit 1
-#redis-cli PING >/dev/null || exit 1
+if [ "$("$(which php)" -v > /dev/null; echo "${?}")" -ne 0 ]; then
+   echo "PHP application not available"
+   exit 1
+fi
+
+SCRIPT_NAME="/status"
+SCRIPT_FILENAME="/usr/local/php/php/fpm/status.html"
+REQUEST_METHOD="GET"
+if [ "$("$(which cgi-fcgi)" -bind -connect "${HOSTNAME}:9000" >/dev/null; echo "${?}")" -ne 0 ]; then
+   echo "FastCGI server not responding"
+   exit 1
+fi
+
+if [ "$("$(which mysql)" --protocol=tcp -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${MYSQL_DATABASE};"; echo "${?}")" -ne 0 ]; then
+   echo "Nextcloud database not available"
+   exit 1
+fi
+
+echo "PHP, FastCGI and Nextcloud database responding OK"
+exit 0
