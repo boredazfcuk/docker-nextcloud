2b869cfd34d54bde65a4b9e35e168189dd7f23ee
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-19 01:28:24 +0000

    Please check change.log

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-if [ "$("$(which php)" -v > /dev/null; echo "${?}")" -ne 0 ]; then
+if [ "$("$(which php)" -v > /dev/null; echo $?)" -ne 0 ]; then
    echo "PHP application not available"
    exit 1
 fi
@@ -8,12 +8,12 @@ fi
 SCRIPT_NAME="/status"
 SCRIPT_FILENAME="/usr/local/php/php/fpm/status.html"
 REQUEST_METHOD="GET"
-if [ "$("$(which cgi-fcgi)" -bind -connect "${HOSTNAME}:9000" >/dev/null; echo "${?}")" -ne 0 ]; then
+if [ "$("$(which cgi-fcgi)" -bind -connect "${HOSTNAME}:9000" >/dev/null; echo $?)" -ne 0 ]; then
    echo "FastCGI server not responding"
    exit 1
 fi
 
-if [ "$("$(which mysql)" --protocol=tcp -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${MYSQL_DATABASE};"; echo "${?}")" -ne 0 ]; then
+if [ "$("$(which mysql)" --protocol=tcp -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "USE ${MYSQL_DATABASE};"; echo $?)" -ne 0 ]; then
    echo "Nextcloud database not available"
    exit 1
 fi
