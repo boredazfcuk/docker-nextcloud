809466923f34615e6fd605a8bc6474007009b310
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-07-22 21:42:18 +0100

    Please check change.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -412,7 +412,7 @@ PrepLaunch "$1"
 if [ -f "/initialise_container" ]; then FirstRun; fi
 ConfigurePHPFPM
 ConfigureSamba
-SetCrontab
+ConfigureCrontab
 SetOwnerAndGroup
 if [ -n "${NEXTCLOUD_TRUSTED_DOMAINS+x}" ]; then SetTrustedDomains; fi
 SetTrustedProxy
